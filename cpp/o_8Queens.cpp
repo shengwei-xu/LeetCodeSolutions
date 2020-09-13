af6dbb2824d76f3475fa08326b1b9ad489d125ff
shengweixu <shengweixu@outlook.com>
2020-09-13 14:57:22 +0800

    format

--- cpp/o_8Queens.cpp
+++ cpp/o_8Queens.cpp
@@ -3,51 +3,62 @@
 #include <cmath>
 using namespace std;
 
-const int N = 8; // number of queens
+const int N = 8;       // number of queens
 int table[N][N] = {0}; // chess table
-int queen[N]; // queens' row position
-int solutions = 0; // number of solutions
+int queen[N];          // queens' row position
+int solutions = 0;     // number of solutions
 
-void init() {
-    for (int i = 0; i < N; ++i) {
-        queen[i] = -1;
-    }
+void init()
+{
+  for (int i = 0; i < N; ++i)
+  {
+    queen[i] = -1;
+  }
 }
 
-int check(int x, int y) { // check whether to put a queen in (x, y)
-    int judge = 1;
-    for (int i = 0; i < N; ++i) {
-        if (queen[i] != -1) {
-            if (x == i || y == queen[i] || 
-                (x - i) == (y - queen[i]) || 
-                (x - i) == -(y - queen[i])) {
-                judge = 0;
-                break;
-            }
-        }
+int check(int x, int y)
+{ // check whether to put a queen in (x, y)
+  int judge = 1;
+  for (int i = 0; i < N; ++i)
+  {
+    if (queen[i] != -1)
+    {
+      if (x == i || y == queen[i] ||
+          (x - i) == (y - queen[i]) ||
+          (x - i) == -(y - queen[i]))
+      {
+        judge = 0;
+        break;
+      }
     }
-    return judge;
+  }
+  return judge;
 }
 
-void dfs(int x) { // x means the queen's row number 
-    if (x > N - 1) { // when x greater than N, it will be a solution here
-        solutions ++;
-        return;
+void dfs(int x)
+{ // x means the queen's row number
+  if (x > N - 1)
+  { // when x greater than N, it will be a solution here
+    solutions++;
+    return;
+  }
+  for (int i = 0; i < N; ++i)
+  {
+    if (check(x, i))
+    {
+      queen[x] = i;  // mark queen position
+      dfs(x + 1);    // go deeper
+      queen[x] = -1; // unmark queen position when back to this layer
     }
-    for (int i = 0; i < N; ++i) {
-        if (check(x, i)) {
-            queen[x] = i; // mark queen position
-            dfs(x + 1); // go deeper
-            queen[x] = -1; // unmark queen position when back to this layer
-        }
-    }
+  }
 }
 
-int main() {
+int main()
+{
 
-    init();
-    dfs(0); // start with #0 row
-    cout << solutions << endl;
+  init();
+  dfs(0); // start with #0 row
+  cout << solutions << endl;
 
-    return 0;
+  return 0;
 }
